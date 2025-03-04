71b42699249ad42a726205058e61eaf94f824374
AMANDA_PAOLA_RAMIREZ_AMAYA <a.22308051281284@cbtis128.edu.mx>
2025-03-04 16:47:21 +0000

    practica autonoma

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Box Decoration"),
+          titleTextStyle:
+              const TextStyle(color: Color(0xffffffff), fontSize: 17),
+          centerTitle: true,
+          backgroundColor: const Color(0xff000000),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              'Amanda Ramirez Mat: 22308051281284',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Center(
+              child: Container(
+                width: 150,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                ),
+                child:
+                    Center(child: Text("Text", style: TextStyle(fontSize: 15))),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Container(
+                width: 150,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child:
+                    Center(child: Text("Text", style: TextStyle(fontSize: 15))),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Container(
+                width: 150,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black54,
+                      offset: new Offset(10.0, 10.0),
+                    ),
+                  ],
+                ),
+                child:
+                    Center(child: Text("Text", style: TextStyle(fontSize: 15))),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Container(
+                width: 150,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 5,
+                  ),
+                  gradient: LinearGradient(
+                    colors: [Colors.white, Colors.black38],
+                  ),
+                ),
+                child: Center(
+                    child: Text("MyText", style: TextStyle(fontSize: 20))),
+              ),
+            ),
+            SizedBox(height: 20.0),
+          ],
         ),
       ),
     );
